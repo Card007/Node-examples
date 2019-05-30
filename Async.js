f558203df4af35aaf9029ea54e26d4c7c4c1f122
Card007 <izd@qq.com>
2019-05-30 19:17:08 +0800

    add async demo

--- Async.js
+++ Async.js
@@ -1,15 +1,15 @@
 // const getWeb = function(){
 //     requestSomething()
 //     setData()
-//     return console.log("程序结束")
+//     return console.log("3.程序结束")
 // }
 
 // const requestSomething = function(){
-//     setTimeout(() => console.log('网页数据爬取完毕！'),2000)
+//     setTimeout(() => console.log('1.网页数据爬取完毕！'),2000)
 // }
 
 // const setData = function(){
-//     console.log('开始处理数据！')
+//     console.log('2.开始处理数据！')
 // }
 
 // getWeb()
@@ -18,25 +18,25 @@
 const getWeb = async function(){
     await requestSomething().catch(e => console.log(e))
     setData()
-    return console.log("程序结束")
+    return console.log("3.程序结束")
 }
 
 const requestSomething = function(){
     return new Promise((resolve, reject) => {
-        let err = '爬取失败！'
-        if(err){
+        // let err = '1.爬取失败！'
+        if(typeof err !=  'undefined'){
             reject(err)
         } else (
-        setTimeout(() => {
-            console.log('网页数据爬取完毕！')
-            resolve()
-        },2000)
+            setTimeout(() => {
+                console.log('1.网页数据爬取完毕！')
+                resolve()
+            },2000)
         )
     })
 }
 
 const setData = function(){
-    console.log('开始处理数据！')
+    console.log('2.开始处理数据！')
 }
 
 getWeb()
